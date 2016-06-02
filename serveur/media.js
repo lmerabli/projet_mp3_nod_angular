921f977ec12d07c9dc9d05a1ef983cfc121be4bd
lmerabli <lmerabli@gmail.com>
2016-06-02 12:40:55 +0200

    rename

--- serveur/media.js
+++ serveur/media.js
@@ -1,9 +1,9 @@
 var express = require('express');
-
+var bdd = requite("./bdd")
 var router = express.Router();
 
 router.get("/",function (req,res){
-    //
+    bdd.getAll
 });
 
 module.export = {};
